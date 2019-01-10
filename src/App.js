3a22b8586822a86dcab187b5a3b05d626a70d36f
Artem Shcherbakov <Artem2431@gmail.com>
2019-01-10 08:43:59 +0200

    Clean-up project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-import React, { Component } from 'react';
-import Welcome from './Welcome'
-import Clock from './Clock'
-import List from './List'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Welcome>World</Welcome>
-        <hr/>
-        <Clock />
-        <List />
+        <h1>Chat</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default App
