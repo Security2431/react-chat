135c346a47201751a8623f241bf4140c837479dc
Artem Shcherbakov <Artem2431@gmail.com>
2019-03-12 18:09:01 +0200

    API-homework fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import configureStore from './store'
 import * as serviceWorker from './serviceWorker'
 
 const rootEl = document.getElementById('root')
 
-ReactDOM.render(<App />, rootEl)
+const store = configureStore()
+
+const render = (Component) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    rootEl
+  )
+}
+
+render(App)
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    ReactDOM.render(<App />, rootEl)
+    render(App)
   })
 }
 
